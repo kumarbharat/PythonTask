a998efa2a184f7d92b571e0b6bd265f158e51141
Bharat Kumar <bharatkr1234@gmail.com>
2020-04-07 09:56:29 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,11 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'prakashkumar30101991@gmail.com'
-DEFAULT_FROM_EMAIL = 'prakashkumar30101991@gmail.com'
-SERVER_EMAIL = 'prakashkumar30101991@gmail.com'
-EMAIL_HOST_PASSWORD = 'prakash@91'
+
